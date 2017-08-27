d11cdde97f818d46c1075f49c2e0a691deabb245
Ibrahim Al-Alali <iboalali@gmail.com>
2017-08-27 23:17:19 +0200

    initial commit

---
+++ app/src/main/java/com/iboalali/sysnotifsnooze/NotificationListener.java
@@ -0,0 +1,10 @@
+package com.iboalali.sysnotifsnooze;
+
+import android.service.notification.NotificationListenerService;
+
+/**
+ * Created by alali on 27-Aug-17.
+ */
+
+public class NotificationListener extends NotificationListenerService {
+}
