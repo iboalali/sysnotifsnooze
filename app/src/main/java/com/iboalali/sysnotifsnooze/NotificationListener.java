cdd0616c4251f614752a0635960397e6cab14465
Ibrahim Al-Alali <iboalali@gmail.com>
2017-10-24 15:45:02 +0200

    preparing for a merge with a pull request

--- app/src/main/java/com/iboalali/sysnotifsnooze/NotificationListener.java
+++ app/src/main/java/com/iboalali/sysnotifsnooze/NotificationListener.java
@@ -38,11 +38,7 @@ public class NotificationListener extends NotificationListenerService {
     public void onNotificationPosted(StatusBarNotification sbn) {
         if (sbn == null)
             return;
-        checkAndSnoozeNotification(sbn);
 
-    }
-
-    private void checkAndSnoozeNotification(StatusBarNotification sbn) {
         if (sbn.getPackageName().equals("android")) {
             Log.d(TAG, sbn.getPackageName() + ": " + sbn.getNotification().extras.getString(getString(R.string.notification_intent_key), ""));
 
@@ -56,18 +52,15 @@ public class NotificationListener extends NotificationListenerService {
                 NotificationListener.this.snoozeNotification(sbn.getKey(), 10000000000000L);
                 Log.d(TAG, sbn.getPackageName() + ": " + key + ", snoozed");
 
-            } else if (Build.MODEL.equals("Pixel 2") || Build.MODEL.equals("Pixel 2 XL")) {
-                if (key.contains(getString(R.string.notification_content_plural_pixel2)) || key.contains(getString(R.string.notification_content_singular_pixel2))) {
-                    NotificationListener.this.snoozeNotification(sbn.getKey(), 10000000000000L);
-                    Log.d(TAG, sbn.getPackageName() + ": " + key + ", snoozed");
-                }
             }
             //Long.MAX_VALUE = 9223372036854775807 = 292.5 million years -> not working
             //10000000000000 = 317.09792 years -> working
+
         }
     }
+
     class NotificationListenerBroadcastReceiver extends BroadcastReceiver{
-    private static final String TAG = "NL Broadcast Receiver";
+        private static final String TAG = "NL Broadcast Receiver";
 
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -75,8 +68,22 @@ public class NotificationListener extends NotificationListenerService {
 
             if(intent.getStringExtra("command").equals("hide")){
                 for(StatusBarNotification sbn: NotificationListener.this.getActiveNotifications()){
-                    checkAndSnoozeNotification(sbn);
 
+                    if (sbn.getPackageName().equals("android")) {
+                        Log.d(TAG, "List: " + sbn.getPackageName() + ": " + sbn.getNotification().extras.getString(getString(R.string.notification_intent_key), ""));
+
+                        String key = sbn.getNotification().extras.getString(getString(R.string.notification_intent_key));
+                        if (key == null) return;
+
+                        String nc = getString(R.string.notification_content_singular);
+                        String ncp = getString(R.string.notification_content_plural);
+
+                        if (key.contains(nc) || key.contains(ncp)) {
+                            NotificationListener.this.snoozeNotification(sbn.getKey(), 10000000000000L);
+                            Log.d(TAG, sbn.getPackageName() + ": " + key + ", snoozed");
+
+                        }
+                    }
                 }
             }
 
